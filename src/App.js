4de1da7c51d048ebfc6f67885c84cb895d6466df
김찬영 <mz_chansm@woowafriends.com>
2022-02-23 20:46:34 +0900

    36 - HTML && CSS 중복사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expense from './components/Expense';
 
 function App() {
   const expense = [
@@ -30,10 +30,7 @@ function App() {
   return (
     <div className="App">
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date} />
-      <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date} />
-      <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date} />
-      <ExpenseItem title={expense[3].title} amount={expense[3].amount} date={expense[3].date} />
+      <Expense items={expense} />
     </div>
   );
 }
